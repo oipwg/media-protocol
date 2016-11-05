2e20000a4f1402967f5b6c29f0d09d3581a0ea35
Jeremiah Buddenhagen <bitspill+github@bitspill.net>
2016-11-05 05:19:43 -0500

    Implement new Parse function

--- protocol.go
+++ protocol.go
@@ -4,10 +4,43 @@ import (
 // "github.com/dloa/media-protocol/messages"
 // "github.com/dloa/media-protocol/utility"
 )
+import (
+	"github.com/dloa/media-protocol/messages"
+	"errors"
+)
 
 const min_block = 1045632
 
 func GetMinBlock() int {
 	// TODO: find min block from multiple protocols programmatically
 	return min_block
+}
+
+func Parse(txComment string, txid string, processingBlock int) (interface{}, map[string]interface{}, error) {
+
+	// check for alexandria-media-multipart single protocol (new media multipart tx-comment)
+	mms, VerifyMediaMultipartSingleError := messages.VerifyMediaMultipartSingle(txComment, txid, processingBlock)
+	if VerifyMediaMultipartSingleError == nil {
+		return mms, nil, nil
+	}
+
+	// check for alexandria-media protocol (new media)
+	media, jmap, VerifyMediaErr := messages.VerifyMedia([]byte(txComment))
+	if VerifyMediaErr == nil {
+		return media, jmap, nil
+	}
+
+	// check for alexandria-media protocol (new publisher)
+	publisher, VerifyPublisherErr := messages.VerifyPublisher([]byte(txComment))
+	if VerifyPublisherErr == nil {
+		return publisher, nil, nil
+	}
+
+	// check for alexandria-media deactivation (new deactivation message)
+	deactivation, VerifyDeactivationError := messages.VerifyDeactivation([]byte(txComment))
+	if VerifyDeactivationError == nil {
+		return deactivation, nil, nil
+	}
+
+	return nil, nil, errors.New("Unknown media type")
 }
