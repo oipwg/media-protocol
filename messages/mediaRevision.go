ac39072fc03664af10b51b2f30bc524aad66ccbd
Jeremiah Buddenhagen <bitspill+github@bitspill.net>
2016-11-12 03:41:43 -0600

    MediaRevision ToDo

---
+++ messages/mediaRevision.go
@@ -0,0 +1,11 @@
+package messages
+
+// ToDo: Allow for editing media without an entire republish
+
+//{
+//  "alexandria-revision": {
+//    "reference": "txID",
+//    "property": "value",
+//    "signature": "sig"
+//  }
+//}
