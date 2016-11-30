ab9bbb6ea989b1719307bfd2e47c41bc81190380
Jeremiah Buddenhagen <bitspill+github@bitspill.net>
2016-11-30 01:57:58 -0600

    Fix media prefix check

--- messages/media.go
+++ messages/media.go
@@ -153,8 +153,11 @@ func VerifyMedia(b []byte) (AlexandriaMedia, map[string]interface{}, error) {
 	var i interface{}
 	var m map[string]interface{}
 
-	if !strings.HasPrefix(string(b), `{ "alexandria-media"`) && !strings.HasPrefix(string(b), `{ "media-data"`) {
-		return v, nil, errors.New("Not alexandria-media")
+	if !strings.HasPrefix(string(b), `{ "alexandria-media"`) &&
+		!strings.HasPrefix(string(b), `{"alexandria-media"`) &&
+		!strings.HasPrefix(string(b), `{"media-data"`) &&
+		!strings.HasPrefix(string(b), `{ "media-data"`) {
+		return v, nil, ErrWrongPrefix
 	}
 
 	if !utility.IsJSON(string(b)) {
