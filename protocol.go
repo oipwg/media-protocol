1a40f4f8d0459d4553d0220515b08fa0a237c858
metacoin <support@colorcoin.org>
2017-08-25 01:31:58 -0400

    added ParseErrors return struct to Parse()
    * Parse() returns a []struct of errors returned from Verify() methods
    * This struct will be printed out iff sync_debug == true and artifact
    type is not detected by Parse
    * Currently this is only used in sync_test.go

--- protocol.go
+++ protocol.go
@@ -10,12 +10,19 @@ import (
 const Version string = "0.4.1"
 const min_block = 1045632
 
+type ParseErrors struct {
+	MessageType string
+	Error       error
+}
+
 func GetMinBlock() int {
 	// TODO: find min block from multiple protocols programmatically
 	return min_block
 }
 
-func Parse(txComment string, txid string, block *flojson.BlockResult, dbtx *sql.Tx) (interface{}, map[string]interface{}, error) {
+func Parse(txComment string, txid string, block *flojson.BlockResult, dbtx *sql.Tx) (interface{}, map[string]interface{}, error, []ParseErrors) {
+
+	var pe []ParseErrors
 
 	if strings.HasPrefix(txComment, "text:") {
 		txComment = txComment[5:]
@@ -26,62 +33,72 @@ func Parse(txComment string, txid string, block *flojson.BlockResult, dbtx *sql.
 	// check for alexandria-media-multipart single protocol (new media multipart tx-comment)
 	mms, VerifyMediaMultipartSingleError := messages.VerifyMediaMultipartSingle(txComment, txid, processingBlock)
 	if VerifyMediaMultipartSingleError == nil {
-		return mms, nil, nil
+		return mms, nil, nil, pe
 	}
+	pe = append(pe, ParseErrors{"MediaMultipartSingle", VerifyMediaMultipartSingleError})
 
 	// check for alexandria-media protocol (new media)
 	media, jmap, VerifyMediaErr := messages.VerifyMedia([]byte(txComment))
 	if VerifyMediaErr == nil {
-		return media, jmap, nil
+		return media, jmap, nil, pe
 	}
+	pe = append(pe, ParseErrors{"AlexandriaMedia", VerifyMediaErr})
 
 	// check for alexandria-media protocol (new publisher)
 	publisher, VerifyPublisherErr := messages.VerifyPublisher([]byte(txComment))
 	if VerifyPublisherErr == nil {
-		return publisher, nil, nil
+		return publisher, nil, nil, pe
 	}
+	pe = append(pe, ParseErrors{"Publisher", VerifyPublisherErr})
 
 	// check for alexandria-media deactivation (new deactivation message)
 	deactivation, VerifyDeactivationError := messages.VerifyDeactivation([]byte(txComment))
 	if VerifyDeactivationError == nil {
-		return deactivation, nil, nil
+		return deactivation, nil, nil, pe
 	}
+	pe = append(pe, ParseErrors{"Deactivation", VerifyDeactivationError})
 
 	// check for historian messages
 	hm, err := messages.VerifyHistorianMessage([]byte(txComment), processingBlock, dbtx)
 	if err == nil {
-		return hm, nil, nil
+		return hm, nil, nil, pe
 	}
+	pe = append(pe, ParseErrors{"Historian", err})
 
 	// check for alexandria-autominer messages
 	am, err := messages.VerifyAutominer([]byte(txComment), processingBlock)
 	if err == nil {
-		return am, nil, nil
+		return am, nil, nil, pe
 	}
+	pe = append(pe, ParseErrors{"Autominer", err})
 
 	// check for alexandria-autominer-pool messages
 	amp, err := messages.VerifyAutominerPool([]byte(txComment), processingBlock)
 	if err == nil {
-		return amp, nil, nil
+		return amp, nil, nil, pe
 	}
+	pe = append(pe, ParseErrors{"AutominerPool", err})
 
 	// check for alexandria-promoter messages
 	promoter, err := messages.VerifyPromoter([]byte(txComment), processingBlock)
 	if err == nil {
-		return promoter, nil, nil
+		return promoter, nil, nil, pe
 	}
+	pe = append(pe, ParseErrors{"Promoter", err})
 
 	// check for alexandria-retailer messages
 	retailer, err := messages.VerifyRetailer([]byte(txComment), processingBlock)
 	if err == nil {
-		return retailer, nil, nil
+		return retailer, nil, nil, pe
 	}
+	pe = append(pe, ParseErrors{"Retailer", err})
 
 	// check for any oip41 data
 	oip041, err := messages.VerifyOIP041(txComment, processingBlock)
 	if err == nil {
-		return oip041, nil, nil
+		return oip041, nil, nil, pe
 	}
+	pe = append(pe, ParseErrors{"OIP041", err})
 
-	return nil, nil, err
+	return nil, nil, err, pe
 }
