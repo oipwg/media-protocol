ba1489db2ffcb80824dd1ee33e89df5542644b39
Jeremiah Buddenhagen <git@bitspill.net>
2018-03-12 15:10:19 -0700

    fix chaincfg.Params

--- utility/floParams.go
+++ utility/floParams.go
@@ -9,14 +9,16 @@ import (
 // Define some of the required parameters for a Florin coin network.
 var FloParams = chaincfg.Params{
 	Name:             "flo",
+	Net:              0xfdc0a5f1,
 	PubKeyHashAddrID: 0x23,
 	ScriptHashAddrID: 0x08,
 }
 
 var FloTestnetParams = chaincfg.Params{
 	Name:             "testFlo",
-	PubKeyHashAddrID: 0x73,
-	ScriptHashAddrID: 0xC6,
+	Net:              0xfdc05af2,
+	PubKeyHashAddrID: 115,
+	ScriptHashAddrID: 198,
 }
 
 func init() {
